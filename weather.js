fc6774bdadaadf7dd6a6e39d42e7de7eaf289c24
vasuahex <knsrinivasareddy@gmail.com>
2023-08-17 20:46:46 +0530

    done weather app

--- weather.js
+++ weather.js
@@ -1,52 +1,94 @@
-let city = document.getElementById("city");
-let type = document.getElementById("type");
-let temp = document.getElementById("temp");
-let image = document.getElementById("img");
-let input = document.getElementById("inp") ;
-let API_key = "6d83156e4e40ca97d0c6924b832fe00c";
+// state
+let currCity = "London";
+let units = "metric";
 
-const data = async function(search){
-   let getData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_key}&units=metric`);
-   console.log(getData);
-   let jsonData =await getData.json();
-   console.log(jsonData);
-   console.log(jsonData.name);
+// Selectors
+let city = document.querySelector(".weather__city");
+let datetime = document.querySelector(".weather__datetime");
+let weather__forecast = document.querySelector('.weather__forecast');
+let weather__temperature = document.querySelector(".weather__temperature");
+let weather__icon = document.querySelector(".weather__icon");
+let weather__minmax = document.querySelector(".weather__minmax")
+let weather__realfeel = document.querySelector('.weather__realfeel');
+let weather__humidity = document.querySelector('.weather__humidity');
+let weather__wind = document.querySelector('.weather__wind');
+let weather__pressure = document.querySelector('.weather__pressure');
 
-   if(jsonData.cod == 400){
-       alert("Please Enter Location")
-       image.src="error1.png";
-       city.innerHTML="";
-       temp.innerHTML="";
-       type.innerHTML="";
-   }
-   if(jsonData.cod == 404){
-       alert("Please Enter correct Location")
-       image.src="error2.png";
-       city.innerHTML=search;
-       temp.innerHTML="";
-       type.innerHTML="";
-   }
-   city.innerHTML=search;
-   temp.innerHTML=Math.floor(jsonData.main.temp)+"°C";
-   type.innerHTML=jsonData.weather[0].main;
+// search
+document.querySelector(".weather__search").addEventListener('submit', e => {
+    let search = document.querySelector(".weather__searchform");
+    // prevent default action
+    e.preventDefault();
+    // change current city
+    currCity = search.value;
+    // get weather forecast 
+    getWeather();
+    // clear form
+    search.value = ""
+})
 
-   if(type.innerHTML == "Clouds"){
-       image.src="Clouds.png"
-   }else if(type.innerHTML == "Clear"){
-       image.src="clears.png"
-   }else if(type.innerHTML == "Rain"){
-       image.src="rain.png"
-   }else if(type.innerHTML == "Snow"){
-       image.src="rain.png"
-   }else if(type.innerHTML == "Haze"){
-       image.src="haze.png"
-   }else if(type.innerHTML == "Strom"){
-       image.src="strom.png"
-   }
-   input.value=""
+// units
+document.querySelector(".weather_unit_celsius").addEventListener('click', () => {
+    if (units !== "metric") {
+        // change to metric
+        units = "metric"
+        // get weather forecast 
+        getWeather()
+    }
+})
+
+document.querySelector(".weather_unit_farenheit").addEventListener('click', () => {
+    if (units !== "imperial") {
+        // change to imperial
+        units = "imperial"
+        // get weather forecast 
+        getWeather()
+    }
+})
+
+function convertTimeStamp(timestamp, timezone) {
+    const convertTimezone = timezone / 3600; // convert seconds to hours 
+
+    const date = new Date(timestamp * 1000);
+
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timeZone: `Etc/GMT${convertTimezone >= 0 ? "-" : "+"}${Math.abs(convertTimezone)}`,
+        hour12: true,
+    }
+    return date.toLocaleString("en-US", options)
+
 }
 
-function myFun(){
-   search=input.value;
-   data(search)
+
+
+// convert country code to name
+function convertCountryCode(country) {
+    let regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+    return regionNames.of(country)
 }
+
+function getWeather() {
+    const API_KEY = '64f60853740a1ee3ba20d0fb595c97d5'
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currCity}&appid=${API_KEY}&units=${units}`).then(res => res.json()).then(data => {
+        console.log(data)
+        city.innerHTML = `${data.name}, ${convertCountryCode(data.sys.country)}`
+        datetime.innerHTML = convertTimeStamp(data.dt, data.timezone);
+        weather__forecast.innerHTML = `<p>${data.weather[0].main}`
+        weather__temperature.innerHTML = `${data.main.temp.toFixed()}&#176`
+        weather__icon.innerHTML = `   <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" />`
+        weather__minmax.innerHTML = `<p>Min: ${data.main.temp_min.toFixed()}&#176</p><p>Max: ${data.main.temp_max.toFixed()}&#176</p>`
+        weather__realfeel.innerHTML = `${data.main.feels_like.toFixed()}&#176`
+        weather__humidity.innerHTML = `${data.main.humidity}%`
+        weather__wind.innerHTML = `${data.wind.speed} ${units === "imperial" ? "mph" : "m/s"}`
+        weather__pressure.innerHTML = `${data.main.pressure} hPa`
+    })
+}
+
+document.body.addEventListener('load', getWeather())
